c8129e6ba215bff7a39d1329c15c123ad0511f58
Uallesson <uallessons@gmail.com>
2021-05-19 14:17:25 -0300

    ..

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -15,37 +15,75 @@ export default NextAuth({
         // ...add more providers here
     ],
     callbacks: {
-        async signIn(user, account, profile) {
-            const { email } = user
-
+        async session(session) {
             try {
-                await fauna.query(
-                    q.If(
-                        q.Not(
-                            q.Exists(
-                                q.Match(
-                                    q.Index('user_by_email'),
-                                    q.Casefold(user.email)
+                const userActiveSubscription = await fauna.query(
+                    q.Get(
+                        q.Intersection([
+                            q.Match(
+                                q.Index('subscription_by_user_ref'),
+                                q.Select(
+                                    'ref',
+                                    q.Get(
+                                        q.Match(
+                                            q.Index('user_by_email'),
+                                            q.Casefold(session.user.email)
+                                        )
+                                    )
                                 )
+                            ),
+                            q.Match(
+                                q.Index('subscription_by_status'),
+                                'active'
                             )
-                        ),
-                        q.Create(
-                            q.Collection('users'),
-                            { data: { email } }
-                        ),
-                        q.Get(
+                        ])
+                    )
+                )
+
+                return {
+                    ...session,
+                    activeSubscription: userActiveSubscription
+                }
+
+            } catch {
+                return {
+                    ...session,
+                    activeSubscription: null,
+                }
+            }
+    },
+    
+    async signIn(user, account, profile) {
+        const { email } = user
+
+        try {
+            await fauna.query(
+                q.If(
+                    q.Not(
+                        q.Exists(
                             q.Match(
                                 q.Index('user_by_email'),
                                 q.Casefold(user.email)
                             )
                         )
+                    ),
+                    q.Create(
+                        q.Collection('users'),
+                        { data: { email } }
+                    ),
+                    q.Get(
+                        q.Match(
+                            q.Index('user_by_email'),
+                            q.Casefold(user.email)
+                        )
                     )
                 )
+            )
 
-                return true
-            } catch {
-                return false
-            }
-        },
-    }
+            return true
+        } catch {
+            return false
+        }
+    },
+}
 })
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,19 +1,74 @@
-import { GetServerSideProps } from "next"
-import { getSession } from "next-auth/client"
-
-
-
-export default function Post() {
-    return (
-        <h1>Ola</h1>
-    )   
+import styles from "./post.module.scss";
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/client";
+import Head from "next/head";
+import { RichText } from "prismic-dom";
+import { getPrismicClient } from "../../services/prismic";
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  };
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-    const session = await getSession({ req })
-
-
-    // if (!session) {
-
-    // }
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div
+            className={styles.postContent}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+        </article>
+      </main>
+    </>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+}) => {
+  const session = await getSession({ req });
+  const { slug } = params;
+
+  if (!session.activeSubscription) {
+    return {
+        redirect: {
+            destination: '/',
+            permanent: false,
+        }
+    }
+  }
+
+  const prismic = getPrismicClient(req);
+  const response = await prismic.getByUID("posts", String(slug), {});
+  
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
